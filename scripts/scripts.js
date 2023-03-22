bc360079f3e8a711127c3b304f6a62c5f393e60f
boyden zhou <boydenz1999@gmail.com>
2023-03-22 12:34:17 -0700

    added time to greeting js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,22 @@
 fname = "dog"
 
-function greet (fname) {
-    console.log('hello, ' + fname)
+function greet (fname, time = "day, ") {
+    console.log('good ' + time + fname)
 }
 
 greet(fname)
+greet (fname, "evening, ")
 
-let input = prompt("what is your name")
-greet (input)
+// let input = prompt("what is your name")
+// greet (input)
+
+// const greet = function (fname)
+// {
+//     console.log (`hello ${fname}`);
+// }
+// greet ("dog");
+
+// const greet = (fname) => 
+// {
+// console.log (`hello ${dog}`)
+// }
